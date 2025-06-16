9138b081dace8632acaa1172b05ac8f1722ef6f5
martinmoneta <mmoneta@uade.edu.ar>
2025-06-16 18:57:10 -0300

    Update burger.js

--- styles/burger.js
+++ styles/burger.js
@@ -1,17 +1,17 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav');
 
-// Evento click tradicional
+
 burger.addEventListener('click', () => {
   nav.classList.toggle('active');
   burger.classList.toggle('toggle');
 
-  // Actualizamos aria-expanded
+
   const expanded = burger.getAttribute('aria-expanded') === 'true';
   burger.setAttribute('aria-expanded', !expanded);
 });
 
-// Agregamos accesibilidad por teclado
+
 burger.addEventListener('keydown', (e) => {
   if (e.key === 'Enter' || e.key === ' ') {
     e.preventDefault();  // Evita scroll por la barra espaciadora
